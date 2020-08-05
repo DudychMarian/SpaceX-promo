ff501b09ebad30b2c38df1980dc0e34874c4ec42
Marian <dudychmarian@gmail.com>
2020-08-05 16:19:00 +0300

    Update project

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var instance = new vidbg('.video', {
+  mp4: 'video/world.mp4', // URL or relative path to MP4 video
+  webm: 'video/world.webm', // URL or relative path to webm video
+  poster: 'img/poster.jpg', // URL or relative path to fallback image
+  overlay: false, // Boolean to display the overlay or not
+});
+var rellax = new Rellax('.rocket');
